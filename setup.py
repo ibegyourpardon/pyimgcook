7c9e9e15d52d3e46125ad84d1392adcfb5de8b14
feiandxs <parala@qq.com>
2020-07-13 16:29:01 +0800

    deployed for the first time

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyimgcook",
-    version="0.0.1",
+    version="0.0.1a1",
     author="ibegyourpardon",
     author_email="yiziyint@gmail.com",
     description="图像处理函数集",
